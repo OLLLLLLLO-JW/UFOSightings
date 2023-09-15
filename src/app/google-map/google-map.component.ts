58b4c8ae7d39cf4db85734beeeacff3fd6c63fe8
Jesse Weis <jesselweis@gmail.com>
2023-09-14 21:32:41 -0700

    map with marker

--- src/app/google-map/google-map.component.ts
+++ src/app/google-map/google-map.component.ts
@@ -16,10 +16,28 @@ export class GoogleMapComponent implements OnInit {
     this.initMap();
   }
 
-  initMap(): void {
-    const map = new google.maps.Map(document.getElementById('map'), {
-      center: { lat: 33.452, lng: -112.074 },
-      zoom: 10
+  async  initMap(): Promise<void> {
+    // const map = new google.maps.Map(document.getElementById('map'), {
+    //   center: { lat: 33.452, lng: -112.074 },
+    //   zoom: 10
+    // });
+    const position = { lat: 33.452, lng: -112.074 };
+    const { Map } = await google.maps.importLibrary("maps");
+    const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+
+    const map = new Map(document.getElementById("map"), {
+      zoom: 10,
+      center: position,
+      mapId: 'anyId',
+      disableDefaultUI: true
     });
+
+    const marker = new AdvancedMarkerElement({
+      map: map,
+      position: position,
+      title: "Ufo"
+    });
+
+
   }
 }
